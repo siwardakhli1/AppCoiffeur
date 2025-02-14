da02b71fb8e496f80f51251086e7414e5aef0185
siwardakhli1 <siwardakhli3@gmail.com>
2025-02-14 11:17:21 +0100

    la partie model

---
+++ src/main/java/com/example/testunit/model/Coiffeur.java
@@ -0,0 +1,10 @@
+package com.example.testunit.model;
+
+public class Coiffeur {
+    private int coiffeurId;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+    private String specialite;
+}
---
+++ src/main/java/com/example/testunit/model/CréneauxHoraires.java
@@ -0,0 +1,11 @@
+package com.example.testunit.model;
+
+import java.time.LocalDateTime;
+
+public class CréneauxHoraires {
+    private int creneauId;
+    private int coiffeurId;
+    private LocalDateTime dateHeureDebut;
+    private LocalDateTime dateHeureFin;
+    private boolean disponible;
+}
